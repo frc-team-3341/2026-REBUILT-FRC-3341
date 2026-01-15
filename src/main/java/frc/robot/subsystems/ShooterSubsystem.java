1ba06b4a87cc63a4022ca6f3021ac1b3f4020c58
Me <ryanh41137@gmail.com>
2026-01-14 20:18:36 -0800

    Create basic code for testing shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,32 +4,76 @@
 
 package frc.robot.subsystems;
 
+// Rev Robotics imports
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+public class ShooterSubsystem extends SubsystemBase {
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  // initialize elements
+  private CANSparkMax shooterWheel;
+  private SparkPIDController shooterWheelController;
+  private RelativeEncoder shooterEncoder;
+
+  // Constants
+  private final int shooterWheelCanId = 4;
+
+  // Variables
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+  public int setPoint; // In RPM
+
+  public ShooterSubsystem() {
+
+    // Initialize motor
+    shooterWheel = new CANSparkMax(shooterWheelCanId, MotorType.kBrushless);
+    shooterWheel.restoreFactoryDefaults();
+
+    // Initialize PID controller and encoder
+    shooterWheelController = shooterWheel.getPIDController();
+    shooterEncoder = shooterWheel.getEncoder();
+
+    // PID Constants
+    kP = 6e-5; 
+    kI = 0;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000015; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
+
+    // set pid values
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // Shuffleboard
+    private ShuffleboardTab tab = Shuffleboard.getTab("Shooter")
+
+    tab.putNumber("P Gain", kP);
+    tab.putNumber("I Gain", kI);
+    tab.putNumber("D Gain", kD);
+    tab.putNumber("I Zone", kIz);
+    tab.putNumber("Feed Forward", kFF);
+    tab.putNumber("Max Output", kMaxOutput);
+    tab.putNumber("Min Output", kMinOutput);
+    tab.putNumber("Set Point", setPoint);
+
+
   }
+  
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
@@ -37,7 +81,28 @@ public class ExampleSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+
+    double p = tab.getNumber("P Gain", 0);
+    double i = tab.getNumber("I Gain", 0);
+    double d = tab.getNumber("D Gain", 0);
+    double iz = tab.getNumber("I Zone", 0);
+    double ff = tab.getNumber("Feed Forward", 0);
+    double max = tab.getNumber("Max Output", 0);
+    double min = tab.getNumber("Min Output", 0);
+
+    // if PID coefficients on shuffleBoard have changed, write new values to controller
+    if((p != kP)) { m_pidController.setP(p); kP = p; }
+    if((i != kI)) { m_pidController.setI(i); kI = i; }
+    if((d != kD)) { m_pidController.setD(d); kD = d; }
+    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      m_pidController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    setPoint = 3000;
+    shooterWheelController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
   }
 
   @Override
