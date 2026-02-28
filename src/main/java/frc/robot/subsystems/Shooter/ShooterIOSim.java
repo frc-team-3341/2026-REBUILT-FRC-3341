1f922a254ce15b0e8afd586048b8f04715a4a04c
will062 <64812612+will062@users.noreply.github.com>
2026-02-27 20:13:42 -0800

    Added shooter sim

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOSim.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.Shooter;
+
+import frc.util.FuelSim;
+
+public class ShooterIOSim implements ShooterIO{
+    
+    FuelSim fuelSim;
+
+
+    public ShooterIOSim(FuelSim fuelSim){
+        this.fuelSim=fuelSim;
+    }
+    
+    public void updateInputs(ShooterIOInputs inputs) {
+
+    }
+
+    public void setFlywheelRPM(double RPM) {
+        
+    }
+
+    public void setFeederSpeed(double speed) {
+
+    }
+
+
+}
