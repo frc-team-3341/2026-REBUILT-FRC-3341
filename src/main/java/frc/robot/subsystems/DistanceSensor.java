4b78c02004ca5054a885e3830f221018a510aedc
Krishna-Robotics <krishnap03312010@gmail.com>
2026-03-07 13:21:19 -0800

    Distance Sensor code
    Distance Sensor code

---
+++ src/main/java/frc/robot/subsystems/DistanceSensor.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DistanceSensor extends SubsystemBase {
+    private final Counter counter;
+    //private double distanceMillimeters;
+
+    
+    public DistanceSensor(int dioPort) {
+        DigitalInput input = new DigitalInput(dioPort);
+        counter = new Counter(input);
+        counter.setSemiPeriodMode(true);
+    }
+
+    public double getDistanceMM() {
+        double pulseWidthMicros = counter.getPeriod() * 1e6;
+        if (pulseWidthMicros > 1850) return -1;
+        double distance = (pulseWidthMicros - 1000) * 3.0 / 4.0;
+        return Math.max(0, distance);
+    }
+
+    public double getDistanceInches() {
+        return getDistanceMM() / 25.4;
+    }
+    
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+        SmartDashboard.putNumber("Millimeters Value", getDistanceMM());
+        SmartDashboard.putNumber("Inches Value", getDistanceInches());
+
+    }
+}
