e19d925aac95e98cefa0a9abd75bfb7030736fa2
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-03-24 18:20:58 -0700

    climber code (buttons unbounded)

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,114 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+
+import edu.wpi.first.wpilibj.PWM;
+
+import com.revrobotics.spark.SparkLimitSwitch;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.ClimberConstants.*;
+
+public class Climber extends SubsystemBase {
+
+    private final SparkFlex leadscrewMotor = new SparkFlex(13, MotorType.kBrushless); 
+    private final RelativeEncoder encoder;
+    private final SparkLimitSwitch topLimitSwitch; // need to check the channels for the limit switches
+    private final SparkLimitSwitch bottomLimitSwitch;
+    private final PWM pwm;
+
+    int pulseTime = 2500;
+
+    public Climber() {
+        pwm = new PWM(0);
+        // servo.setAngle(0);
+
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kBrake);
+        // config.softLimit
+            // .forwardSoftLimit(SOFT_LIMIT_TOP)
+            // .forwardSoftLimitEnabled(true)
+            // .reverseSoftLimit(SOFT_LIMIT_BOTTOM)
+            // .reverseSoftLimitEnabled(true);
+        config.limitSwitch
+            .forwardLimitSwitchType(Type.kNormallyClosed)
+            .reverseLimitSwitchType(Type.kNormallyClosed);
+
+        leadscrewMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+        encoder = leadscrewMotor.getEncoder();
+        encoder.setPosition(0);
+
+        topLimitSwitch = leadscrewMotor.getForwardLimitSwitch();
+        bottomLimitSwitch = leadscrewMotor.getReverseLimitSwitch();
+
+        SmartDashboard.putNumber("pulseTime", pulseTime);
+
+    }
+
+    // Leadscrew movement
+    public Command leadscrewUp() {
+        return this.runOnce(() -> {
+            leadscrewMotor.set(SPEED);
+        }).alongWith(deploy());
+    }
+
+    public Command leadscrewDown() {
+        return this.runOnce(() -> {
+            leadscrewMotor.set(-SPEED);
+            
+        }).alongWith(deploy());
+    }
+
+    public Command leadscrewStop() {
+        return this.runOnce(() -> leadscrewMotor.set(0.0));
+    }
+
+    // Encoder utilities
+    public double getPosition() {
+        return encoder.getPosition();
+    }
+
+    public void resetEncoder() {
+        encoder.setPosition(0);
+    }
+
+    public Command setPulseTime() {
+        return this.runOnce(() -> {
+            System.out.println(pulseTime);
+            pwm.setPulseTimeMicroseconds(pulseTime);
+        });
+    }
+
+    public void setAngle(double angle) {
+        pwm.setPulseTimeMicroseconds((int) ((1000)+angle*5.55));
+    }
+
+    public Command deploy() {
+        return Commands.runOnce(() ->pwm.setPulseTimeMicroseconds(2000));
+    }
+
+    public Command retract() {
+        return Commands.runOnce(() -> pwm.setPulseTimeMicroseconds(1400));
+    }
+
+    @Override
+    public void periodic() {
+        
+        SmartDashboard.putNumber("Position", encoder.getPosition());
+        SmartDashboard.putBoolean("TopLimitSwitch", topLimitSwitch.isPressed());
+        SmartDashboard.putBoolean("BottomLimitSwitch", bottomLimitSwitch.isPressed());
+
+        pulseTime = (int) SmartDashboard.getNumber("pulseTime", pulseTime);
+        
+    }
+}
